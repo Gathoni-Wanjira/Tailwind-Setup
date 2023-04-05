bea93d5cdd9434ab82bd5dc06345dd8980807b3f
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-05 21:27:52 +0300

    added utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+/**
+ * The Tailwind config file allows you to customize various aspects of the framework,
+ *  such as color palettes, font families, spacing, breakpoints, and more. 
+ * By modifying the Tailwind config file, 
+ * you can create a custom set of CSS classes that matches your project's specific design requirements.
+ */
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem"
+    },
   },
   plugins: [],
 }
